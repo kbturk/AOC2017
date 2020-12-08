da547b00fd8cbd61d700440e9240cc885b1c6844
Katherine Turk <kbturk87@gmail.com>
2020-12-08 15:11:16 -0600

    shorted day 2

--- day2.py
+++ day2.py
@@ -9,13 +9,8 @@ def main( args ):
         print(sum( [ max(x) - min(x) for x in l ] ))
 
         #part 2:
-        ans2 = []
-        for row in l:
-            for i in range( len( row ) - 1 ):
-                for j in range(i+1,len( row )):
-                    if row[i]%row[j] == 0:
-                        ans2.append(row[i]/row[j])
-        print(sum(ans2))
+        print( sum( [ a/b for row in l for a,b in itertools.combinations(row,2) if a%b == 0 ]))
+
 
 if __name__ == '__main__':
     sys.exit( main( sys.argv ) )
