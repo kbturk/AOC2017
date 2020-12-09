97d3720e650003bdca21d87c37ad2dd708ec410e
Katherine Turk <kbturk87@gmail.com>
2020-12-09 17:52:25 -0600

    day 6 & day 7 solutions

--- day6.py
+++ day6.py
@@ -6,12 +6,12 @@ def reorder_blocks(l):
     blocks = l[reorder_spot]
     l[reorder_spot] = 0
 
-    while blocks != 0:
+    while blocks:
         reorder_spot += 1
         if reorder_spot > len(l)-1:
             reorder_spot = 0
         l[reorder_spot] += 1
-        blocks += -1
+        blocks -= 1
     return(l)
 
 def main( args ):
@@ -24,27 +24,13 @@ def main( args ):
     count = 0
     while l not in seen:
         seen.append(deepcopy(l))
-        #print(f'running with: {l}')
-        #print(f'seen is: {seen}')
-
         reorder_blocks(l)
-
-        #print(f'l is now: {l}')
-        #print(f'seen is: {seen}')
-
         count += 1
 
     print(count, l)
 
     #part 2
-    count = 1
-    ans = deepcopy(l)
-    l = reorder_blocks(l)
-
-    while l != ans:
-        reorder_blocks(l)
-        count += 1
-    print(count, l)
+    print(len(seen)-seen.index(l))
 
 if __name__ == '__main__':
     sys.exit( main( sys.argv ) )
--- day7.py
+++ day7.py
@@ -1,28 +1,50 @@
 import sys, itertools
 from copy import deepcopy
 
+from typing import Dict, List, Optional, Set, Tuple
 
-def main( args ):
+def tot_weight(key: str, d: Dict[str, Tuple[int, Set[str]]]) -> int:
+    child_weight = [ tot_weight(item,d) for item in d[key][1] ]
+    #print(child_weight)
+    if len(set(child_weight)) > 1:
+        print(f"issue found. {key}'s children {d[key][1]} have different weights: {child_weight}. one of these is off")
+        l = list(zip(d[key][1], child_weight))
+        for i in range(len(child_weight)):
+            if l[i][1] == l[i+1][1]:
+                continue
+            elif l[i][1] == l[i+2][1]:
+                continue
+            else:
+                diff = - abs(l[i][1] - l[i-1][1])
+                name, off = l[i]
+                print(f'{name} is off by {diff}, weight should be: {d[name][0]+ diff}')
+                break
+    mass_weight = d[key][0] + sum(child_weight)
+    return mass_weight
+
+def main(args: List[str]) -> int:
     with open( args[1], 'r' ) as f:
         l = [line.strip().split('->') for line in f ]
-        d = {}
-        #leaves = []
+        d: Dict[str, Tuple[int, Set[str]]] = {}
+        #part1:
         for entry in l:
             key, weight =entry[0].split()[0], int(entry[0].split()[1].strip('()'))
             if len(entry) > 1:
                 dependencies = set(entry[1].strip().split(', '))
-                #leaves.append(entry[1].strip().split(', '))
             else:
-                dependencies = None
-            d[key] = [weight,dependencies]
-        #leaves = [c for a in leaves for c in a]
-        leaves = [c for value in d.values() if value[1] != None for c in value[1]]
-
+                dependencies = set()
+            d[key] = (weight,dependencies)
+        leaves = [c for value in d.values() for c in value[1]]
         for key in d.keys():
             if key not in leaves:
-                print(f'bottom reached with {key}.')
+                bottom = key
+                print(f'bottom reached with {bottom}.')
 
+        #part2:
+        #print(d)
+        tot_weight(bottom,d)
 
+    return 0
 
 if __name__ == '__main__':
     sys.exit( main( sys.argv ) )
